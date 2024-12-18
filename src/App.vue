7cd69fc78937d1849cf25afb94994bf5005ebb1d
libao <2946183082@qq.com>
2024-12-18 23:20:25 +0800

    恢复vue初始页面

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,6 @@
 <template>
   <div id="app">
     <router-view/>
-    <van-button type="primary">主要按钮</van-button>
-    <van-switch v-model="checked" />
-    <van-rate v-model="value" />
   </div>
 
 </template>
@@ -12,8 +9,7 @@
 export default {
   data () {
     return {
-      checked: true,
-      value: 5
+
     }
   }
 }
