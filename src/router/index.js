59da7a720b212b10bc96d05aef7954a5f3444153
libao <2946183082@qq.com>
2024-12-18 23:21:05 +0800

    配置路由信息

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,24 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Login from '@/views/login'
+import Layout from '@/views/layout'
+import Myorder from '@/views/myorder'
+import Pay from '@/views/pay'
+import ProDetail from '@/views/prodetail'
+import Search from '@/views/search/index.vue'
+import List from '@/views/search/list.vue'
 
 Vue.use(VueRouter)
 
-const routes = []
+const routes = [
+  { path: '/login', component: Login },
+  { path: '/', component: Layout },
+  { path: '/myorder', component: Myorder },
+  { path: '/pay', component: Pay },
+  { path: '/prodetail/:id', component: ProDetail },
+  { path: '/search', component: Search },
+  { path: '/searchlist', component: List }
+]
 
 const router = new VueRouter({
   routes
